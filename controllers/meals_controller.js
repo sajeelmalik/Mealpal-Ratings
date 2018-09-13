2caa92ea7532f853054d4b78d9913a387b416148
Sajeel Malik <sajeel23@berkeley.edu>
2018-09-12 20:24:09 -0700

    set up handlebars and controller routing

---
+++ controllers/meals_controller.js
@@ -0,0 +1,65 @@
+//Sajeel Malik
+//Create routing paths for the server using Express
+
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (meal.js) to use its database functions
+var meal = require("../models/meal.js");
+
+
+router.get("/", function(req, res) {
+    meal.selectAll(function(data) {
+      var hbsObject = {
+        meals: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/api/meals", function(req, res) {
+    meal.insertOne([
+      "name", "sleepy"
+    ], [
+      req.body.name, req.body.sleepy
+    ], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/meals/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    meal.updateOne({
+      sleepy: req.body.sleepy
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+  router.delete("/api/meals/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    meal.delete(condition, function(result) {
+      if (result.affectedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
+  
+  // Export routes for server.js to use.
+  module.exports = router;
+  
